1f8b592fd27250dfb79e8970af5f130a5f80febc
Crashsol <47147551@qq.com>
2018-09-11 10:32:08 +0800

    添加HeroService服务，并通过构造器注入到HeroexComponent中，HeroService是一个异步操作

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +12,18 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
   // 英雄列表
-  heroes = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  // 构造器注入HeroService
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getAllHeroes();
+  }
+
+  // 定义获取Heroes方法,
+  // 该方法返回的是一个可观察对象，（异步操作）
+  getAllHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   // 选择当前Hero
