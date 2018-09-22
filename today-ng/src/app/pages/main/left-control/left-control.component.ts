e512bb364aa034e3329aa785137a98375ea0bc20
Crashsol <47147551@qq.com>
2018-09-22 11:26:17 +0800

    添加左侧任务列表添加

--- today-ng/src/app/pages/main/left-control/left-control.component.ts
+++ today-ng/src/app/pages/main/left-control/left-control.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { LocalStorageService } from '../../../services/local-storage.service';
 import { USERNAME } from '../../../services/local-storage.namespace';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-left-control',
@@ -9,12 +10,19 @@ import { USERNAME } from '../../../services/local-storage.namespace';
 })
 export class LeftControlComponent implements OnInit {
   username: string;
+  @Input() isCollapsed: boolean;
+  @ViewChild(ListComponent) listComponent: ListComponent;
   constructor(
     private store: LocalStorageService
   ) { }
 
   ngOnInit() {
     this.username = this.store.get(USERNAME);
+  }
+
+  openAddListModal(): void {
+    // 在Angular中调用子组件
+    this.listComponent.openAddListModal();
   }
 
 }
--- today-ng/src/app/pages/main/left-control/list/list.component.ts
+++ today-ng/src/app/pages/main/left-control/list/list.component.ts
@@ -1,15 +1,116 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, OnDestroy, TemplateRef } from '@angular/core';
+import { List } from 'src/domain/entities';
+import { NzDropdownContextComponent, NzDropdownService, NzModalService } from 'ng-zorro-antd';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ListService } from 'src/app/services/list/list.service';
+import { TodoService } from 'src/app/services/todo/todo.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.less']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
+  constructor(
+    private dropdownService: NzDropdownService,
+    private listService: ListService,
+    private todoService: TodoService,
+    private modal: NzModalService) { }
 
-  constructor() { }
+  @Input() isCollapsed: boolean;
+  @ViewChild('listRenameInput') private listRenameInput: ElementRef;
+  @ViewChild('listInput') private listInput: ElementRef;
+
+  lists: List[];
+  currentListUuid: string;
+  contextListUuid: string;
+  addListModalVisible = false;
+  renameListModalVisible = false;
+  private dropdown: NzDropdownContextComponent;
+  private destroy$ = new Subject();
 
   ngOnInit() {
+    this.listService.lists$.
+      pipe(takeUntil(this.destroy$))
+      .subscribe(lists => {
+        this.lists = lists;
+      });
+
+    this.listService.currentUuid$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(uuid => {
+        this.currentListUuid = uuid;
+      });
+    this.listService.getAll();
   }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+
+  closeAddListModal(): void {
+    this.addListModalVisible = false;
+  }
+
+  closeRenameListModal(): void {
+    this.renameListModalVisible = false;
+  }
+
+  openAddListModal(): void {
+    this.addListModalVisible = true;
+    setTimeout(() => {
+      this.listInput.nativeElement.focus();
+    });
+  }
+
+  openRenameListModal(): void {
+    this.renameListModalVisible = true;
+    setTimeout(() => {
+      const title = this.lists.find(b => b._id === this.contextListUuid).title;
+      console.log(title);
+      this.listRenameInput.nativeElement.value = title;
+      this.listRenameInput.nativeElement.focus();
+    });
+  }
+  // 创建菜单
+  contextMenu($event: MouseEvent, template: TemplateRef<void>, uuid: string): void {
+    this.dropdown = this.dropdownService.create($event, template);
+  }
+
+  click(uuid: string): void {
+    this.listService.setCurrentUuid(uuid);
+  }
+
+  rename(title: string): void {
+    this.listService.rename(this.contextListUuid, title);
+    this.closeRenameListModal();
+  }
+
+  add(title: string): void {
+    this.listService.add(title);
+    this.closeAddListModal();
+  }
+
+  delete(): void {
+    const uuid = this.contextListUuid;
+    this.modal.confirm({
+      nzTitle: '确认删除列表',
+      nzContent: '该操作会导致该列表下的所有待办事项被删除',
+      nzOnOk: () =>
+        new Promise((res, rej) => {
+
+          this.listService.delete(uuid);
+          this.todoService.deleteInList(uuid);
+          res();
+        }).catch(() => { console.log('Delete list Failed'); })
+    });
+  }
+
+  close(): void {
+    this.dropdown.close();
+  }
+
+
 
 }
