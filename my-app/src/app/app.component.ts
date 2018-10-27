45722b5490c26fa150ac239177b94034b42cf094
crashsol <47147551@qq.com>
2018-10-27 15:43:36 +0800

    sss

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Person } from './models/Person';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+
+  title = 'My Frist App';
+  me: Person = {
+    name: 'Crashol',
+    age: 12
+  };
+
+
 }
